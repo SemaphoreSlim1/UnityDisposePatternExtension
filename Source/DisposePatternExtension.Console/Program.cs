2ca8b3d10e4aa633bcae6f7474fa9aa519911037
Matthew Thomas <matthewethomas@users.noreply.github.com>
2016-05-05 04:49:37 +0000

    Showing explicit vs auto-container disposing

--- Source/DisposePatternExtension.Console/Program.cs
+++ Source/DisposePatternExtension.Console/Program.cs
@@ -10,19 +10,42 @@ namespace DisposePatternExtension.Console
     {
         static void Main(string[] args)
         {
+            UsingDispose();
+        }
+
+        private static void ExplicitDispose()
+        {
             //instantiate the container and the extension
             var container = new UnityContainer();
             container.AddNewExtension<TypeWeaveExtension>();
 
             container.RegisterType<ICustomObject, CustomObject>(new WeaveInjectionMember<IDisposable, DisposeBase>());
-           
+
             var obj = container.Resolve<ICustomObject>();
 
             obj.DoSomething();
 
-            if(obj is IDisposable)
+            if (obj is IDisposable)
             {
                 (obj as IDisposable).Dispose();
+            }
+
+            System.Console.WriteLine("Boo Ya");
+            System.Console.ReadLine();
+        }
+
+        private static void UsingDispose()
+        {
+            using (var container = new UnityContainer())
+            {
+                container.AddNewExtension<TypeWeaveExtension>();
+
+                container.RegisterType<ICustomObject, CustomObject>(new ContainerControlledLifetimeManager(),
+                    new WeaveInjectionMember<IDisposable, DisposeBase>());
+
+                var obj = container.Resolve<ICustomObject>();
+
+                obj.DoSomething();
             }
 
             System.Console.WriteLine("Boo Ya");
